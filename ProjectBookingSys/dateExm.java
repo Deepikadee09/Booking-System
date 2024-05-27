6684626d67173be6576cb749dd7a542de178f74a
Deepikadee09 <123984311+Deepikadee09@users.noreply.github.com>
2024-05-27 10:47:40 +0530

    Add files via upload

---
+++ ProjectBookingSys/dateExm.java
@@ -0,0 +1,42 @@
+
+
+class DateDemo {
+       static final String ANSI_RESET = "\u001B[0m";
+       static final String ANSI_BLACK = "\u001B[30m";
+       static final String ANSI_RED = "\u001B[31m";
+       static final String ANSI_GREEN = "\u001B[32m";
+       static final String ANSI_YELLOW = "\u001B[33m";
+       static final String ANSI_BLUE = "\u001B[34m";
+       static final String ANSI_PURPLE = "\u001B[35m";
+       static final String ANSI_CYAN = "\u001B[36m";
+       static final String ANSI_WHITE = "\u001B[37m";
+   public static void main(String args[]) {
+     
+      System.out.printf("%50s%s kjhk%n", ,  );
+ System.out.printf("%s\f%s!%n","Hello","World!");
+
+
+
+
+
+// System.out.println(ANSI_WHITE);
+// System.out.println("Hello World");
+// System.out.println(ANSI_RESET);
+// System.out.println("Hello World");
+// System.out.println(ANSI_BLACK);
+// System.out.println(ANSI_RED);
+// System.out.println("Hello World");
+// System.out.println(ANSI_GREEN);
+// System.out.println("Hello World");
+// System.out.println(ANSI_YELLOW);
+// System.out.println("Hello World");
+// System.out.println(ANSI_BLUE);
+// System.out.println("Hello World");
+// System.out.println(ANSI_PURPLE);
+// System.out.println("Hello World");
+// System.out.println(ANSI_CYAN + "Hello World");
+// System.out.println("Hello World");
+    
+   }
+}
+
